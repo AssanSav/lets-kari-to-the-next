3b665c3a549398173c6d4cd53f8d2c5168133076
AssanSav <assane.savadogo81@gmail.com>
2020-06-15 00:44:09 -0400

    Login, session status and destroy actions added

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,65 +1,46 @@
-class SessionsController < ApplicationController
+class Api::V1::SessionsController < ApplicationController
 
-    def signup 
-        user = User.create!(user_params)
-        if user.valid?
-            session[:user_id] = user.id
-            render json: {
-                status: 200,
-                user: UserSerializer.new(user)
-            }
-        else 
-            render json: {
-                status: 409,
-                error: user.errors.full_messages
-            }
-        end
+  def login 
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      render json: {
+        status: 200,
+        user: UserSerializer.new(user),
+        interests: user.interests
+      }
+    else 
+      render json: {
+        status: 409,
+        error: user.errors.full_messages
+      }
     end
+  end
 
-    def login 
-        user = User.find_by(email: params[:email])
-        if user && user.authenticate(params[:password])
-            session[:user_id] = user.id
-            render json: {
-                status: 200,
-                user: UserSerializer.new(user)
-            }
-        else 
-            render json: {
-                status: 409,
-                error: user.errors.full_messages
-            }
-        end
+  def is_logged_in?
+    if !!session[:user_id] && current_user
+      render json: {
+        logged_in: true,
+        user: UserSerializer.new(current_user),
+        interests: current_user.interests
+      }
+    else
+        render json: {
+          logged_in: false,
+          message: "No Such User!"
+        }
     end
+  end
 
-    def is_logged_in?
-        if !!session[:user_id] && current_user
-            render json: {
-                logged_in: true,
-                user: UserSerializer.new(current_user)
-            }
-        else
-            render json: {
-                logged_in: false,
-                message: "No Such User!"
-            }
-        end
+  def destroy 
+    user = User.find(session[:user_id])
+    if user 
+      session.clear
+      render json: {
+        status: 200,
+        logout: true
+      }
     end
+  end
 
-    def destroy 
-        user = User.find(session[:user_id])
-        if user 
-            session.clear
-            render json: {
-                status: 200,
-                logout: true
-            }
-        end
-    end
-
-    private 
-
-    def user_params 
-        params.require(:session).permit(name, username, password, password_confirmation)
-    end
 end
