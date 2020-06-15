60e8a7685c25728f34a999ea0afd196518967e0b
AssanSav <assane.savadogo81@gmail.com>
2020-06-15 00:41:36 -0400

    Create plus inbox and outbox actions for user text message management

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,2 +1,42 @@
-class MessagesController < ApplicationController
+class Api::V1::MessagesController < ApplicationController
+    
+    def create 
+      binding.pry
+      message = current_user.messsages.create!(message_params)
+      if message 
+        render json: {
+          status: 200,
+          message: message
+        }
+      else 
+        render json: {
+          status: 409,
+          error: message.errors.full_messages
+        }
+      end
+    end
+
+    def outbox
+      messages = current_user.received_messages
+      if messages 
+        render json: {
+          status: 200,
+          received_messages: messages
+        }
+      else 
+        render json: {
+          status: 500,
+          message: ["No Record Found!"]
+        }
+      end
+    end
+
+    def inbox
+    end
+
+    private 
+
+    def message_params
+      params.require(:message).permit(:content, :user_id, :match_id)
+    end
 end
