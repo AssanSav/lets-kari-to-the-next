bfce3d162d44c4832839bf10cdde4e3e885a3e83
AssanSav <assane.savadogo81@gmail.com>
2020-06-15 00:38:21 -0400

    helper method added

--- app/controllers/api/v1/interests_controller.rb
+++ app/controllers/api/v1/interests_controller.rb
@@ -1,2 +1,9 @@
-class InterestsController < ApplicationController
+class Api::V1::InterestsController < ApplicationController
+
+  def index
+    render json: {
+      interests: Interest.all
+    }
+  end
+
 end
