637bf2b1303aabde9b8d36a3778ee431125ce66c
AssanSav <assane.savadogo81@gmail.com>
2020-06-18 16:43:39 -0400

    Active storage macro association method added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,20 +1,20 @@
 class User < ApplicationRecord
+    has_one_attached :avatar
+
     has_secure_password
     has_many :user_interests
     has_many :interests, through: :user_interests
     has_many :sent_messages, class_name: "Message"
     has_many :received_messages, class_name: "Message", foreign_key: "match_id" 
 
-    validates_presence_of :username, :email, :image, :age, :city, :gender, :orientation, :ethnicity, :height, :body_shape, :children, :relationship, :education, :bio
+    validates_presence_of :username, :email, :age, :city, :gender, :orientation, :ethnicity, :height, :body_shape, :children, :relationship, :education, :bio
     validates_uniqueness_of :email, :username
     validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 
     before_create :set_default_avatar
 
     def set_default_avatar
-        if self.image == ""
-        self.image = "https://cdn1.iconfinder.com/data/icons/ninja-things-1/1772/ninja-simple-512.png"
-        end
+      self.image = "https://cdn1.iconfinder.com/data/icons/ninja-things-1/1772/ninja-simple-512.png"
     end
 
   def find_my_matches 
