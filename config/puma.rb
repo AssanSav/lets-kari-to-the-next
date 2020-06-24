f1d51591eed3a82dfbfaca04f564e8d4d78028fc
AssanSav <assane.savadogo81@gmail.com>
2020-06-24 15:46:57 -0400

    Deploy test

--- config/puma.rb
+++ config/puma.rb
@@ -10,7 +10,23 @@ threads min_threads_count, max_threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
-port        ENV.fetch("PORT") { 3001 }
+# port        ENV.fetch("PORT") || { 3001 }
+
+# workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+# threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
+# threads threads_count, threads_count
+
+# preload_app!
+
+# rackup      DefaultRackup
+port        ENV['PORT']     || 3001
+# environment ENV['RACK_ENV'] || 'development'
+
+# on_worker_boot do
+#   # Worker specific setup for Rails 4.1+
+#   # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
+#   ActiveRecord::Base.establish_connection
+# end
 
 # Specifies the `environment` that Puma will run in.
 #
