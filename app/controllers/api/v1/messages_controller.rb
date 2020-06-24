f8ed2a105dfec17f138e21a62ff3d42338fe1185
AssanSav <assane.savadogo81@gmail.com>
2020-06-23 20:06:59 -0400

    Update received and sent messages

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -17,11 +17,11 @@ class Api::V1::MessagesController < ApplicationController
     end
 
     def outbox
-      messages = current_user.sent_messages
+      messages = current_user.sent_messages.where(sent_visible: true)
       if messages 
         render json: {
           status: 200,
-          sent_messages: MessageSerializer.new(current_user.sent_messages),
+          sent_messages: MessageSerializer.new(messages),
         }
       else 
         render json: {
@@ -32,11 +32,11 @@ class Api::V1::MessagesController < ApplicationController
     end
 
     def inbox
-      messages = current_user.received_messages
+      messages = current_user.received_messages.where(received_visible: true)
       if messages
         render json: {
           status: 200,
-          received_messages: MessageSerializer.new(current_user.received_messages),
+          received_messages: MessageSerializer.new(messages)
         }
       else 
         render json: {
@@ -46,9 +46,39 @@ class Api::V1::MessagesController < ApplicationController
       end
     end
 
+    def update_send_message 
+      message = current_user.sent_messages.find(params[:id]) 
+      if message.update(content: params[:content], sent_visible: false)
+        render json: {
+          status: 200,
+          message: message
+        }
+      else 
+        render json: {
+          status: 500,
+          errors: message.errors.full_messages
+        }
+      end
+    end
+
+    def update_received_message 
+      message = current_user.received_messages.find(params[:id]) 
+      if message.update(content: params[:content], received_visible: false)
+        render json: {
+          status: 200,
+          message: message
+        }
+      else 
+        render json: {
+          status: 500,
+          errors: message.errors.full_messages
+        }
+      end
+    end
+
     private 
 
     def message_params
-      params.require(:message).permit(:content, :user_id, :match_id)
+      params.require(:message).permit(:content, :visible, :user_id, :match_id)
     end
 end
