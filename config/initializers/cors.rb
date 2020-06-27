197f1646c1ce6b4e1bec585df27248e31bf43584
AssanSav <assane.savadogo81@gmail.com>
2020-06-26 22:24:00 -0400

    Refact

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do 
   allow do
-    origins "https://lets-kari-to-the-next.herokuapp.com"
+    origins "https://lets-meetup.netlify.app"
     # 'http://localhost:3000'
   
     resource '*',
