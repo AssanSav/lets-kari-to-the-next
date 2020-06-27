2c60fc92ddcaa91e418a18c11fb718306456a810
AssanSav <assane.savadogo81@gmail.com>
2020-06-27 18:19:49 -0400

    Refact

--- config/application.rb
+++ config/application.rb
@@ -2,10 +2,10 @@ require_relative 'boot'
 require 'rails/all'
 require "active_storage/engine"
 
-Bundler.require(*Rails.groups)
-Dotenv::Railtie.load
+# Bundler.require(*Rails.groups)
+# Dotenv::Railtie.load
 
-HOSTNAME = ENV['HOSTNAME']
+# HOSTNAME = ENV['HOSTNAME']
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
