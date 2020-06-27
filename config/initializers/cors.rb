3af611d2c31ae1b82236558c5b1d432c434f12c4
AssanSav <assane.savadogo81@gmail.com>
2020-06-27 00:21:10 -0400

    Setting up Cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,8 +1,8 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do 
   allow do
-    origins "*"
+    origins ENV['FRONT_END_URL']
     # ["https://lets-meetup.netlify.app"
-    # 'http://localhost:3000'
+    # 
   
     resource '*',
       headers: :any,
