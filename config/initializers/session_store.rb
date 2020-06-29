3e6fee7a47b44619c5cdc2357e1ce270d073512c
AssanSav <assane.savadogo81@gmail.com>
2020-06-29 18:15:04 -0400

    Refact

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,9 @@
 
 
-if Rails.env == 'production' 
-  Rails.application.config.session_store :cookie_store, key: '_lets-kari-to-the-next', domain: 'https://lets-meetup-app.herokuapp.com/login'
-else
+# if Rails.env == 'production' 
+  # Rails.application.config.session_store :cookie_store, key: '_lets-kari-to-the-next'
+  # , domain: 'https://lets-meetup-app.herokuapp.com/login'
+# else
   Rails.application.config.session_store :cookie_store, key: '_lets-kari-to-the-next' 
-end
+# end
 
