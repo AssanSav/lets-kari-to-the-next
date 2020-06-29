4d6c476012f7ce92b4337c9ba67d2d1f7d99df0c
AssanSav <assane.savadogo81@gmail.com>
2020-06-29 19:02:34 -0400

    postgresql table dropped

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,9 +1,4 @@
 
+Rails.application.config.session_store :cookie_store, key: '_lets-kari-to-the-next' 
 
-# if Rails.env == 'production' 
-  # Rails.application.config.session_store :cookie_store, key: '_lets-kari-to-the-next'
-  # , domain: 'https://lets-meetup-app.herokuapp.com/login'
-# else
-  Rails.application.config.session_store :cookie_store, key: '_lets-kari-to-the-next' 
-# end
 
