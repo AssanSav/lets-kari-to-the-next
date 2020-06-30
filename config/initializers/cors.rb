7ece3edcf8a304516c016324ebf995264a53fa56
AssanSav <assane.savadogo81@gmail.com>
2020-06-29 21:59:21 -0400

    Deploy on heroku

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,8 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do 
   allow do
-    origins 'http://localhost:3000'
-    #
+    origins "https://lets-meetup-app.herokuapp.com"
+    
+    # 'http://localhost:3000'
     # "https://lets-meetup-app.herokuapp.com"
     
     resource '*',
