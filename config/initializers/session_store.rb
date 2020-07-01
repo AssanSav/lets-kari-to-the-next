fe83fee437551ef4c8da06daefa98921e6ebd0f6
AssanSav <assane.savadogo81@gmail.com>
2020-06-30 22:59:11 -0400

    Deploy on heroku

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,8 @@
 
-Rails.application.config.session_store :cookie_store, key: '_lets-kari-to-the-next' 
 
+if Rails.env == 'production' 
+  Rails.application.config.session_store :cookie_store, key: '_lets-kari-to-the-next', domain: 'https://lets-meetup-app.herokuapp.com'
+else
+  Rails.application.config.session_store :cookie_store, key: '_lets-kari-to-the-next' 
+end
 
